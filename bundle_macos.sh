d5b3093e3b2f28e1130134a509c4c4791c1730a0
Leslie Kerman <kerbal255@outlook.com>
2026-01-13 09:17:09 +0000

    Added app icon for macOS bundle

--- bundle_macos.sh
+++ bundle_macos.sh
@@ -5,14 +5,12 @@ set -xe
 SOFTRAS_DEFAULT_ASSET_PATH="../Resources/assets.respack.bin" cargo build --quiet --release --package softras_winit_wgpu --target x86_64-apple-darwin
 SOFTRAS_DEFAULT_ASSET_PATH="../Resources/assets.respack.bin" cargo build --quiet --release --package softras_winit_wgpu --target aarch64-apple-darwin
 
-mkdir -p "target/Software Rasterizer.app/Contents/"
-mkdir -p "target/Software Rasterizer.app/Contents/MacOS/"
-mkdir -p "target/Software Rasterizer.app/Contents/Resources/"
+rm -rf "target/Software Rasterizer.app"
+cp -r macos_bundle_template "target/Software Rasterizer.app"
 
-lipo -create -output target/Software\ Rasterizer.app/Contents/MacOS/softras_winit_wgpu \
-    target/aarch64-apple-darwin/release/softras_winit_wgpu                             \
-    target/x86_64-apple-darwin/release/softras_winit_wgpu
+lipo -create -output "target/Software Rasterizer.app/Contents/MacOS/softras_winit_wgpu" \
+    "target/aarch64-apple-darwin/release/softras_winit_wgpu"                            \
+    "target/x86_64-apple-darwin/release/softras_winit_wgpu"
 
-cp macos_bundle_info.plist.xml "target/Software Rasterizer.app/Contents/Info.plist"
-cp assets.respack.bin "target/Software Rasterizer.app/Contents/Resources/assets.respack.bin"
+cp "assets.respack.bin" "target/Software Rasterizer.app/Contents/Resources/assets.respack.bin"
 
